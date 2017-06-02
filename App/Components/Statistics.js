8d596e587c533317419021045428394c429c8aa8
Yubin Kim <joeekimm@gmail.com>
2017-06-02 09:49:20 -0700

    added statistics page (still working on it)

--- App/Components/Statistics.js
+++ App/Components/Statistics.js
@@ -1,27 +1,100 @@
-//import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  TouchableWithoutFeedback,
+  View
+} from "react-native";
 
-// create a component
-class Statistics extends Component {
+import AreaSpline from "./PieChart/AreaSpline";
+import PieChart from "./PieChart/PieChart";
+import Theme from "./PieChart";
+import data from "./PieChart/data";
+
+type State = {
+  activeIndex: number,
+  spendingsPerYear: any
+};
+
+export default class App extends Component {
+  state: State;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+      spendingsPerYear: data.spendingsPerYear
+    };
+    this._onPieItemSelected = this._onPieItemSelected.bind(this);
+    this._shuffle = this._shuffle.bind(this);
+  }
+
+  _onPieItemSelected(newIndex) {
+    this.setState({
+      ...this.state,
+      activeIndex: newIndex,
+      spendingsPerYear: this._shuffle(data.spendingsPerYear)
+    });
+  }
+
+  _shuffle(a) {
+    for (let i = a.length; i; i--) {
+      let j = Math.floor(Math.random() * i);
+      [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+    return a;
+  }
+
   render() {
+    const height = 200;
+    const width = 500;
+
     return (
-      <View style={styles.container}>
-        <Text>Statistics</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.chart_title}>
+            Distribution of spending this month
+          </Text>
+          <PieChart
+            pieWidth={150}
+            pieHeight={150}
+            onItemSelected={this._onPieItemSelected}
+            colors={Theme.colors}
+            width={width}
+            height={height}
+            data={data.spendingsLastMonth}
+          />
+          <Text style={styles.chart_title}>
+            Spending per year in
+            {" "}
+            {data.spendingsLastMonth[this.state.activeIndex].name}
+          </Text>
+          <AreaSpline
+            width={width}
+            height={height}
+            data={this.state.spendingsPerYear}
+            color={Theme.colors[this.state.activeIndex]}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
 
-// define your styles
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50',
+    backgroundColor: "whitesmoke",
+    marginTop: 85
   },
-});
-
-//make this component available to the app
-export default Statistics;
+  chart_title: {
+    paddingTop: 15,
+    textAlign: "center",
+    paddingBottom: 5,
+    paddingLeft: 5,
+    fontSize: 25,
+    backgroundColor: "white",
+    color: "grey",
+    fontWeight: "bold"
+  }
+};
