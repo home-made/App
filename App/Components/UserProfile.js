9cfb8d5e2046e88924be71c8b7c3568901ace728
jaimemendozadev <jaimemendozadev@gmail.com>
2017-06-04 15:18:56 -0700

    Fix broken profile pic on UserProfile.js

--- App/Components/UserProfile.js
+++ App/Components/UserProfile.js
@@ -66,17 +66,26 @@ export default class Profile extends Component {
 
   componentWillMount() {
     let context = this;
-    let userId;
 
     async function grabAuthId() {
       try {
         const profile = await AsyncStorage.getItem("profile");
         if (profile !== null && profile !== undefined) {
+          console.log("profile inside UserProfile.js is ", JSON.parse(profile))
           userId = JSON.parse(profile).userId;
-          console.log(JSON.parse(profile).userId);
-          console.log(userId);
 
-          SetProfile(context, userId);
+          parsedProfile = JSON.parse(profile);
+
+          var userPic = parsedProfile.picture_large ? parsedProfile.picture_large : parsedProfile.picture;
+          var authId = parsedProfile.userId;
+          var fullName = parsedProfile.name;
+
+          context.setState({
+            userPic, fullName
+          });
+
+          SetProfile(context, authId)
+   
         }
       } catch (err) {
         console.log("Error getting profile: ", err);
@@ -86,6 +95,7 @@ export default class Profile extends Component {
   }
 
   render() {
+    console.log("the state inside UserProfile is ", this.state)
     return (
       <Container style={{ marginTop: 60 }}>
         <Content>
@@ -122,9 +132,9 @@ export default class Profile extends Component {
                       borderRadius: 60
                     }}
                     source={{
-                      uri: !this.state.profileUrl
+                      uri: !this.state.userPic
                         ? ""
-                        : this.state.user.profileUrl
+                        : this.state.userPic
                     }}
                   />
                 </Row>
@@ -148,3 +158,86 @@ const styles = StyleSheet.create({
     alignItems: "center"
   }
 });
+
+
+/*
+
+
+gmail UserProfile
+
+
+
+{"extraInfo":
+  {"locale":"en","family_name":"Mendoza","gender":"male","email_verified":true,"given_name":"Jaime","updated_at":"2017-06-04T19:30:37.883Z","clientID":"Rp7ThYPPRNHrSGUaLOv_Ub307zwDb_VR","global_client_id":"Yr3FRkbN-xAd7_KvxSZ-6Fvz6OqRPXHM"},
+
+  "userMetadata":{},
+  "picture":"https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg",
+  "userId":"google-oauth2|111548799256401715601",
+  "identities":[{"social":true,"provider":"google-oauth2","identityId":"google-oauth2|111548799256401715601","userId":"111548799256401715601","connection":"google-oauth2"}],
+  "appMetadata":{},
+  "nickname":"jaimemendozadev",
+  "email":"jaimemendozadev@gmail.com",
+  "name":"Jaime Mendoza",
+  "createdAt":1495800977.404,
+  "chefView":false,
+  "isChef":false}
+
+
+
+facebook profile
+
+{"extraInfo":
+  {"third_party_id":"-R4vav3zP0MHd52x_pp6UuVdwr0",
+  "context":{"mutual_likes":{"summary":{"total_count":100},"data":[]},"id":"dXNlcl9jb250ZAXh0OgGQNl5YJU9UckwPGo2bIRs3LKZAPSxf2G2ln42xXj2JvGMycwlHm0WZAbIcRonlkYVq0ABZCHorWgUnyg5fZCehqnZAM6dcZCcr24qtBZBxgw1dQ5pYpIZD"},
+  
+  "link":"https://www.facebook.com/app_scoped_user_id/10209561963947713/",
+  "name_format":"{first} {last}",
+  "clientID":"Rp7ThYPPRNHrSGUaLOv_Ub307zwDb_VR",
+  "updated_at":"2017-06-04T20:19:02.741Z",
+  "global_client_id":"Yr3FRkbN-xAd7_KvxSZ-6Fvz6OqRPXHM",
+  "family_name":"Susan",
+  "is_verified":false,
+  "devices":[{"hardware":"iPhone","os":"iOS"}],
+  "installed":true,
+  "locale":"en_US",
+  "verified":true,
+  "email_verified":true,
+  "given_name":"David",
+  "gender":"male",
+  "timezone":-7,
+  "age_range":{"min":21},
+  "picture_large":"https://scontent.xx.fbcdn.net/v/t1.0-1/15380389_10208254679826427_3585448135424856175_n.jpg?oh=b3564cd79c481bb06a20f0905257927e&oe=59AEC34F",
+  "updated_time":"2017-04-25T19:49:46+0000"},
+  "picture":"https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/15380389_10208254679826427_3585448135424856175_n.jpg?oh=253da281d806851d8599885520b03ab0&oe=59B31CD3",
+  "userId":"facebook|10209561963947713",
+  "identities":[{"social":true,"provider":"facebook","identityId":"facebook|10209561963947713","userId":"10209561963947713","connection":"facebook"}],
+  "appMetadata":{},
+  "nickname":"David Susan",
+  "createdAt":1495740860.409,
+  "name":"David Susan",
+  "userMetadata":{},
+  "isChef":true,
+  "chefView":true}
+
+
+
+regular email profile
+
+{"extraInfo":
+  {"email_verified":false,"updated_at":"2017-06-04T20:34:55.215Z","clientID":"Rp7ThYPPRNHrSGUaLOv_Ub307zwDb_VR","global_client_id":"Yr3FRkbN-xAd7_KvxSZ-6Fvz6OqRPXHM"},
+
+   "userMetadata":{},
+   "picture":"https://s.gravatar.com/avatar/245cf079454dc9a3374a7c076de247cc?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fte.png",
+   "userId":"auth0|59346eee5f874258e3ffb4c5",
+   "identities":[{"social":false,"provider":"auth0","identityId":"auth0|59346eee5f874258e3ffb4c5","userId":"59346eee5f874258e3ffb4c5","connection":"Username-Password-Authentication"}],
+   "appMetadata":{},
+   "nickname":"test1",
+   "email":"test1@gmail.com",
+   "name":"test1@gmail.com",
+   "createdAt":1496608494.933,
+   "chefView":false,"isChef":false}
+
+*/
+
+
+
