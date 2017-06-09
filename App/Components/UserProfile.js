d2532bdffb4c5cb672af33e9feb8c83a1dfcbc29
enjsmoove <Sankhara@Davids-iMac.local>
2017-06-09 13:57:29 -0700

    customer profiles looking good

--- App/Components/UserProfile.js
+++ App/Components/UserProfile.js
@@ -17,6 +17,7 @@ import {
   Button,
   H3
 } from "native-base";
+import Icon from "react-native-vector-icons/Entypo";
 import { Actions, ActionConst } from "react-native-router-flux";
 import { Grid, Row, Col } from "react-native-easy-grid";
 import DishView from "./DishView";
@@ -108,6 +109,25 @@ export default class Profile extends Component {
             chefReviews: user.data[0].chefReviews,
             customerReviews: user.data[0].customerReviews,
             status: user.data[0].status
+          },()=>{
+            let scoresArray = [];
+            let numOfReviews = this.state.user.chefReviews.length;
+
+            let reviews = this.state.user.chefReviews.map(curr => {
+              scoresArray.push(curr.score);
+              
+            });
+            console.log('score is',scoresArray)
+
+            let avgScore = 0;
+            if (scoresArray.length > 0) {
+               avgScore = scoresArray.reduce((a, b) => a + b);
+               avgScore = (avgScore / numOfReviews).toPrecision(2);
+           }
+           console.log(avgScore)
+           
+            this.setState({ avgScore });
+          // });
           });
         })
         .catch(error => {
@@ -118,130 +138,93 @@ export default class Profile extends Component {
         });
     }
   }
-
+  returnStar() {
+    return <Icon style={{ color: "#EFEF54", fontSize: 18 }} name="star" />;
+  }
   render() {
     const styles = {
       text: {
         color: "#505050",
         alignSelf: "center"
+      },
+      container: {
+        marginTop: 60
+      },
+      cardName: {
+        alignSelf: "center",
+        fontSize: 18,
+        color: "#505050",
+        fontFamily: "Noteworthy-Bold"
+      },
+      cardDesc: {
+        alignSelf: "center",
+        fontSize: 14,
+        fontFamily: "Noteworthy-light"
+      },
+      cardRating: {
+        alignSelf: "center",
+        fontSize: 14,
+        fontFamily: "Noteworthy-light"
+      },
+      cardRow: {
+        justifyContent: "center",
+        alignSelf: "center"
+      },
+      cardImage: {
+        width: 120,
+        height: 120,
+        justifyContent: "center",
+        borderRadius: 60
       }
     };
 
     return (
-      <Content>
-        <Card style={{ flex: 0.3, marginTop: -50, justifyContent: "center" }}>
-          <CardItem>
-            <Body>
-              <Text style={styles.text}>
-                {!this.state.fullName ? "name unknown" : this.state.fullName}
-              </Text>
-              <Text style={styles.text} note>
-                {!this.state.status
-                  ? "No status at this time."
-                  : this.state.status}
-              </Text>
-            </Body>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Row style={{ justifyContent: "center", alignSelf: "center" }}>
-                <Image
-                  style={{
-                    position: "absolute",
-                    width: 120,
-                    height: 120,
-                    justifyContent: "center",
-                    borderRadius: 60
-                  }}
-                  source={{
-                    uri: !this.state.userPic ? "" : this.state.userPic
-                  }}
-                />
-              </Row>
-            </Body>
-          </CardItem>
-        </Card>
+      <Container>
+        <Container style={styles.container}>
+          <Content>
+            <Card style={{ flex: 0.3, marginTop: -150, justifyContent: "center" }}>
+              <CardItem>
+                <Body style={{ justifyContent: "center", alignItems: "center" }}>
+                  <Text style={styles.cardName}>
+                    {!this.state.fullName ? "name unknown" : this.state.fullName}
+                  </Text>
+                  <Text style={styles.cardDesc} note>
+                    {!this.state.status
+                      ? "No status at this time."
+                      : this.state.status}
+                  </Text>
+                   {this.state.avgScore > 0
+                  ? <Text style={styles.cardRating} note>
+                      {this.returnStar()} {this.state.avgScore}
+                    </Text>
+                  : <Text note>No Reviews Available</Text>}
+                </Body>
+              </CardItem>
+              <CardItem>
+                <Body>
+                  <Row style={{ justifyContent: "center", alignSelf: "center" }}>
+                    <Image
+                      style={{
+                        position: "absolute",
+                        width: 120,
+                        height: 120,
+                        justifyContent: "center",
+                        borderRadius: 60
+                      }}
+                      source={{
+                        uri: !this.state.userPic ? "" : this.state.userPic
+                      }}
+                    />
+                  </Row>
+                </Body>
+              </CardItem>
+            </Card>
 
-        {this.handleReviews()}
+            {this.handleReviews()}
 
-      </Content>
+          </Content>
+        </Container>
+      </Container>
     );
   }
 }
-
-/*
-gmail UserProfile
-
-
-
-{"extraInfo":
-  {"locale":"en","family_name":"Mendoza","gender":"male","email_verified":true,"given_name":"Jaime","updated_at":"2017-06-04T19:30:37.883Z","clientID":"Rp7ThYPPRNHrSGUaLOv_Ub307zwDb_VR","global_client_id":"Yr3FRkbN-xAd7_KvxSZ-6Fvz6OqRPXHM"},
-
-  "userMetadata":{},
-  "picture":"http://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg",
-  "userId":"google-oauth2|111548799256401715601",
-  "identities":[{"social":true,"provider":"google-oauth2","identityId":"google-oauth2|111548799256401715601","userId":"111548799256401715601","connection":"google-oauth2"}],
-  "appMetadata":{},
-  "nickname":"jaimemendozadev",
-  "email":"jaimemendozadev@gmail.com",
-  "name":"Jaime Mendoza",
-  "createdAt":1495800977.404,
-  "chefView":false,
-  "isChef":false}
-
-
-
-facebook profile
-
-{"extraInfo":
-  {"third_party_id":"-R4vav3zP0MHd52x_pp6UuVdwr0",
-  "context":{"mutual_likes":{"summary":{"total_count":100},"data":[]},"id":"dXNlcl9jb250ZAXh0OgGQNl5YJU9UckwPGo2bIRs3LKZAPSxf2G2ln42xXj2JvGMycwlHm0WZAbIcRonlkYVq0ABZCHorWgUnyg5fZCehqnZAM6dcZCcr24qtBZBxgw1dQ5pYpIZD"},
-  
-  "link":"http://www.facebook.com/app_scoped_user_id/10209561963947713/",
-  "name_format":"{first} {last}",
-  "clientID":"Rp7ThYPPRNHrSGUaLOv_Ub307zwDb_VR",
-  "updated_at":"2017-06-04T20:19:02.741Z",
-  "global_client_id":"Yr3FRkbN-xAd7_KvxSZ-6Fvz6OqRPXHM",
-  "family_name":"Susan",
-  "is_verified":false,
-  "devices":[{"hardware":"iPhone","os":"iOS"}],
-  "installed":true,
-  "locale":"en_US",
-  "verified":true,
-  "email_verified":true,
-  "given_name":"David",
-  "gender":"male",
-  "timezone":-7,
-  "age_range":{"min":21},
-  "picture_large":"http://scontent.xx.fbcdn.net/v/t1.0-1/15380389_10208254679826427_3585448135424856175_n.jpg?oh=b3564cd79c481bb06a20f0905257927e&oe=59AEC34F",
-  "updated_time":"2017-04-25T19:49:46+0000"},
-  "picture":"http://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/15380389_10208254679826427_3585448135424856175_n.jpg?oh=253da281d806851d8599885520b03ab0&oe=59B31CD3",
-  "userId":"facebook|10209561963947713",
-  "identities":[{"social":true,"provider":"facebook","identityId":"facebook|10209561963947713","userId":"10209561963947713","connection":"facebook"}],
-  "appMetadata":{},
-  "nickname":"David Susan",
-  "createdAt":1495740860.409,
-  "name":"David Susan",
-  "userMetadata":{},
-  "isChef":true,
-  "chefView":true}
-
-
-
-regular email profile
-
-{"extraInfo":
-  {"email_verified":false,"updated_at":"2017-06-04T20:34:55.215Z","clientID":"Rp7ThYPPRNHrSGUaLOv_Ub307zwDb_VR","global_client_id":"Yr3FRkbN-xAd7_KvxSZ-6Fvz6OqRPXHM"},
-
-   "userMetadata":{},
-   "picture":"http://s.gravatar.com/avatar/245cf079454dc9a3374a7c076de247cc?s=480&r=pg&d=http%3A%2F%2Fcdn.auth0.com%2Favatars%2Fte.png",
-   "userId":"auth0|59346eee5f874258e3ffb4c5",
-   "identities":[{"social":false,"provider":"auth0","identityId":"auth0|59346eee5f874258e3ffb4c5","userId":"59346eee5f874258e3ffb4c5","connection":"Username-Password-Authentication"}],
-   "appMetadata":{},
-   "nickname":"test1",
-   "email":"test1@gmail.com",
-   "name":"test1@gmail.com",
-   "createdAt":1496608494.933,
-   "chefView":false,"isChef":false}
-
-*/
